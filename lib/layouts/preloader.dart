5cae40f5cd4c01aa9886c407c79a2102408f8504
CovenantJunior <covenanttope@gmail.com>
2023-10-06 11:21:49 +0100

    Preloader

--- lib/layouts/preloader.dart
+++ lib/layouts/preloader.dart
@@ -1,1 +1,17 @@
 import 'package:flutter/material.dart';
+
+class Preloader extends StatefulWidget {
+  const Preloader({super.key});
+
+  @override
+  State<Preloader> createState() => _PreloaderState();
+}
+
+class _PreloaderState extends State<Preloader> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text('Loading...'),
+    );
+  }
+}
