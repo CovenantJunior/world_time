bf56d69893a87d9e82e3f5bfccc2b0fdb43a72a6
CovenantJunior <covenanttope@gmail.com>
2023-10-09 20:45:09 +0100

    initState() bedrock for initiating http request

--- lib/layouts/preloader.dart
+++ lib/layouts/preloader.dart
@@ -8,6 +8,13 @@ class Preloader extends StatefulWidget {
 }
 
 class _PreloaderState extends State<Preloader> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
