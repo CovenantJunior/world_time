cb384f018fb0e80c0e44ae57484acc2631d9cd8b
CovenantJunior <covenanttope@gmail.com>
2023-10-16 18:39:57 +0100

    A bit padding and flutter_spinkit

--- lib/layouts/home.dart
+++ lib/layouts/home.dart
@@ -20,20 +20,50 @@ class _HomeState extends State<Home> {
 
     return Scaffold(
       body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(0, 200, 0, 0),
           child: Column(
+            // mainAxisAlignment: MainAxisAlignment.center,
             children: [
               TextButton(
                 onPressed: () {
                   Navigator.pushNamed(context, '/choose_location');
                 },
                 child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
                     Icon(Icons.edit_location),
                     Text('Choose Location'),
                   ],
                 ),
               ),
-          ],
+              const SizedBox(
+                height: 0
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    data['location'],
+                    style: const TextStyle(
+                      color: Colors.blue,
+                      fontSize: 30
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                data['time'],
+                style: const TextStyle(
+                  color: Colors.blue,
+                  fontSize: 60
+                ),
+              ),
+            ],
+          ),
         )
       ),
     );
