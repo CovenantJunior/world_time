3a300dc8d7ad6aa57d538f0e38a788ad444f9952
CovenantJunior <covenanttope@gmail.com>
2023-10-28 14:14:13 +0100

    FIle asset loading

--- lib/layouts/home.dart
+++ lib/layouts/home.dart
@@ -44,7 +44,7 @@ class _HomeState extends State<Home> {
           )
         ),
         child: Padding(
-          padding: const EdgeInsets.fromLTRB(0, 200, 0, 0),
+          padding: const EdgeInsets.fromLTRB(0, 300, 0, 0),
           child: Column(
             // mainAxisAlignment: MainAxisAlignment.center,
             children: [
